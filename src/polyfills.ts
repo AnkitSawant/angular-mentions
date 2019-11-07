75bf4285ea8c0bea9c457bd808572041499083cd
Ankit Sawant <AnkitS7@citiustech.com>
2019-11-07 18:55:47 +0530

    ie tried 2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,19 +20,29 @@
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es7/set';
+
+import 'core-js/features/set';
+import 'core-js/features/promise';
+import 'core-js/features/regexp';
+import 'core-js/features/parse-int';
+import 'core-js/features/function';
+import 'core-js/features/number';
+import 'core-js/features/string';
+import 'core-js/features/array';
+import 'core-js/features/object';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
